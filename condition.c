a2a474828efa854d9f02893ee87ad90b5efb46d3
Mr.Psych <121602380+bhanunaroju@users.noreply.github.com>
2023-03-10 19:45:35 +0530

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int M;
+    printf("Enter value of M :- ");
+    scanf("%d",&M);
+    if(M%3==0 & M%5==0){
+        printf("Good Number");
+    }else{
+        if(M%3==0 & M%5 !=0){
+            printf("Bad Number");
+        }else{
+            if(M%5==0 & M%3 !=0){
+                printf("Poor Number");
+            }else{
+                printf("-1");
+            }
+        }
+    }
+    return 0;
+}
